ec481ff934e65bdd313bf2370495bdcea2015197
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2023-10-24 22:29:40 -0300

    Tipos e Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar a classe String que representa texto na aplicação
+
+        String meuNome = "DHI CARVALHO";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //Cast
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 3.15;
+        System.out.println(meuNome);
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto2);
+        System.out.println(VALOR_DE_PI);
     }
 }
